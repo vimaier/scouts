de36ffa7e80ab2faab2ae6a027afff2b995be75b
RBoelter <robo0001@stud.fh-kl.de>
2015-09-29 00:40:23 +0200

    blabla

--- src/main/java/com/sap/innojam/scouts/api/DummyData.java
+++ src/main/java/com/sap/innojam/scouts/api/DummyData.java
@@ -9,21 +9,23 @@ import com.sap.innojam.scouts.entity.TalentCategory;
 import com.sap.innojam.scouts.entity.Upload;
 
 public class DummyData {
-	
-	public static Scout scott = new Scout("uid", "artistName", "Scott", "Scoutsen", 35, "m", "scott@scoutsen.com", null, null);
+
+	public static Scout scott = new Scout("uid", "artistName", "Scott", "Scoutsen", 35, "m", "scott@scoutsen.com",
+			null, null);
 	public static List<Upload> dummyData = new ArrayList<Upload>();
 	public static TalentCategory catPainting = new TalentCategory("Painting");
 	public static TalentCategory catSinging = new TalentCategory("Singing");
 	public static TalentCategory catDancing = new TalentCategory("Dancing");
-	public static Talent dummyTalent = new Talent("ted@talent.com");
+	public static Talent dummyTalent = new Talent("123", "The Queen of England", "Elizabeth", "Alexandra Mary", 130,
+			"female", "queen@buckingham.uk", null, null);
 	public static Upload dummyUploadImg = new Upload(1, dummyTalent, catPainting, Upload.Type.Image, "jpg");
 	public static Upload dummyUploadAudio = new Upload(2, dummyTalent, catSinging, Upload.Type.Audio, "mp3");
 	public static Upload dummyUploadVideo = new Upload(3, dummyTalent, catDancing, Upload.Type.Video, "mp4");
-	static{
+	static {
 		dummyTalent.getUploads().add(dummyUploadImg);
 		dummyTalent.getUploads().add(dummyUploadAudio);
 		dummyTalent.getUploads().add(dummyUploadVideo);
 		dummyData = dummyTalent.getUploads();
 	}
-	
+
 }
--- src/main/java/com/sap/innojam/scouts/api/TalentAPI.java
+++ src/main/java/com/sap/innojam/scouts/api/TalentAPI.java
@@ -1,6 +1,8 @@
 package com.sap.innojam.scouts.api;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -14,71 +16,74 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 
 import com.sap.innojam.scouts.dao.SensorDAO;
-import com.sap.innojam.scouts.entity.SensorData;
+import com.sap.innojam.scouts.entity.Talent;
 import com.sap.innojam.scouts.entity.Upload;
+import com.sap.security.um.user.PersistenceException;
+import com.sap.security.um.user.UnsupportedUserAttributeException;
+import com.sap.security.um.user.User;
+import com.sap.security.um.user.UserProvider;
 
 @Path("/talent")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class TalentAPI {
 
+	Logger LOGGER = Logger.getLogger(TalentAPI.class.getName());
+
 	@Inject
 	SensorDAO dao;
-	
+
+	@Inject
+	UserProvider userProvider;
+
 	@Context
 	SecurityContext request;
 
 	@GET
 	@Path("/{uid}")
-	public List<SensorData> getTalent(@PathParam("uid") String userId) {
-		//TODO: add stub talent
-//		return dao.findAllByOwnerAndDevice(request.getUserPrincipal().getName(), device);
-		return null;
+	public Talent getTalent(@PathParam("uid") String userId) {
+		LOGGER.info("execute getTalent with userId = " + userId);
+		return DummyData.dummyTalent;
 	}
 
 	@GET
 	@Path("/uploads")
 	public List<Upload> getAllUploads() {
-		//TODO: return upload ids (or better get-urls) of the user
-//		return dao.findAllByOwnerAndDevice(request.getUserPrincipal().getName(), device);
-		return null;
+		LOGGER.info("execute getAllUploads() for currentUser");
+		try {
+			User currentUser = userProvider.getCurrentUser();
+			String userId = currentUser.getAttribute("userid");
+			LOGGER.info("***** execute getAllUploads for user = " + userId);
+		} catch (PersistenceException | UnsupportedUserAttributeException e) {
+			e.printStackTrace();
+		}
+		List<Upload> ret = new ArrayList<Upload>();
+		ret.add(DummyData.dummyUploadAudio);
+		ret.add(DummyData.dummyUploadImg);
+		ret.add(DummyData.dummyUploadVideo);
+		return ret;
 	}
 
-//	@POST
-//	@Path("/{device}")
-//	public SensorData create(@PathParam("device") String device, SensorData sensorParam) {
-//		SensorData newSensor = new SensorData(request.getUserPrincipal().getName(), device, new Date(), sensorParam.getType(),
-//				sensorParam.getValue());
-//		return dao.insert(newSensor);
-//	}
 	@POST
 	@Path("/create")
-	public SensorData createTalent(SensorData sensorParam) {
-//		SensorData newSensor = new SensorData(request.getUserPrincipal().getName(), device, new Date(), sensorParam.getType(),
-//				sensorParam.getValue());
-//		return dao.insert(newSensor);
-		// TODO: create a talent and return it
-		return null;
+	public boolean createTalent(@PathParam("talent") Talent talent) {
+		LOGGER.info("execute createTalent for currentUser");
+		// not important for prototype
+		return false;
 	}
 
 	@GET
 	@Path("/accept/{scoutid}")
-	public JsonResponse acceptScoutRequest(@PathParam("scoutid") String scoutId) {
-		//TODO: get uid of current talent
-		//TODO: check if scoutId is in list of TalentRequests of curr Talent
-		//TODO: if yes change state to 'accepted'
-		//TODO: send/save notification
-		return null;
+	public void acceptScoutRequest(@PathParam("scoutid") String scoutId) {
+		LOGGER.info("execute acceptScoutRequest for currentUser+ " + scoutId);
+		// not important for prototype
 	}
-	
+
 	@GET
 	@Path("/reject/{scoutid}")
 	public JsonResponse rejectScoutRequest(@PathParam("scoutid") String scoutId) {
-		//TODO: get uid of current talent
-		//TODO: check if scoutId is in list of TalentRequests of curr Talent
-		//TODO: if yes change state to 'rejected'
-		//TODO: send/save notification
+		LOGGER.info("execute rejectScoutRequest for currentUser+ " + scoutId);
+		// not important for prototype
 		return null;
 	}
-	
 }
