e526c42b682de7ba1059c1fb20ad4f7172901861
RBoelter <robo0001@stud.fh-kl.de>
2015-09-29 06:26:08 +0200

    rdfdegfgdsgsdf

--- src/main/java/com/sap/innojam/scouts/entity/Like.java
+++ src/main/java/com/sap/innojam/scouts/entity/Like.java
@@ -1,18 +1,16 @@
 package com.sap.innojam.scouts.entity;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 @XmlRootElement
 public class Like {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
@@ -29,17 +27,14 @@ public class Like {
 		this.upload = upload;
 	}
 
-	@XmlTransient
 	public long getId() {
 		return id;
 	}
 
-	@XmlTransient
 	public User getLiker() {
 		return liker;
 	}
 
-	@XmlTransient
 	public Upload getUpload() {
 		return upload;
 	}
