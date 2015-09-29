6b86e140cc580fe4f5b231015b9af854223ceaaa
Simon Könnecke <simonkoennecke@gmail.com>
2015-09-29 02:48:11 +0200

    audio video

--- src/main/webapp/view/Welcome.controller.js
+++ src/main/webapp/view/Welcome.controller.js
@@ -1,9 +1,8 @@
 sap.ui.define(['jquery.sap.global',
 		'sap/ui/model/json/JSONModel',
-		'sap/ui/core/mvc/Controller',
-		'sap/ui/core/HTML'
+		'sap/ui/core/mvc/Controller'
 		],
-	function(jQuery, JSONModel, Controller, HTML) {
+	function(jQuery, JSONModel, Controller) {
 	"use strict";
 
 	var CController = Controller.extend("friststep.view.Welcome", {
@@ -25,19 +24,20 @@ sap.ui.define(['jquery.sap.global',
 			var that = this;
 			jQuery.getJSON("./api/upload/random", function(data) {
 				var oModel = new JSONModel(data);
-				oModel.path = "/backend/" + oModel.path;
+				oModel.path = oModel.path;
 				that.getView().setModel(oModel);
-				
-				
-				var html1 = new HTML("html1", {
-                content:
-                        "<video width='50%' height='50%' autoplay>" +
-                        "<source src='/backend/" + oModel.path + "' type='video/mp4'>" +
-                        "Your browser does not support the video tag." +
-                        "</video>"
-    			});
-    			
-    			that.getView().getContent().append(html1);
+				var element = that.getView().byId("videoPlayer");
+				if (data.type === "Video") {  
+					element.setContent("<video width='50%' height='50%' autoplay controls src='" + data.path + "'>" +
+	                        "Your browser does not support the video tag." +
+	                        "</video>");
+				} else if (data.type === "Audio") {
+					element.setContent("<audio width='50%' height='50%' autoplay controls src='" + data.path + "'>" +
+	                        "Your browser does not support the video tag." +
+	                        "</audio>");
+				} else if (data.type === "Image") {
+					element.setContent("<img width='50%' height='50%' src='" + data.path + "'/>");
+				}
     			
 			});
 			
